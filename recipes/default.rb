e36468f7827a18314ccc77c6c07de8c5e993f382
Mariano Rodriguez <martemars2000@gmail.com>
2018-10-29 11:57:47 -0300

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,56 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+execute 'Add new php repository' do
+    command 'sudo add-apt-repository ppa:ondrej/php'
+    action :run
+end
+
+execute 'Update ubuntu repositories' do
+    command 'sudo apt-get update'
+    action :run
+end
+
+package 'git'
+package 'tree'
+
+package 'php7.1' do
+    action :install
+end
+
+package 'php7.1-common' do
+    action :install
+end
+
+package 'php7.1-curl' do
+    action :install
+end
+
+package 'php7.1-xml' do
+    action :install
+end
+
+package 'php7.1-zip' do
+    action :install
+end
+
+package 'php7.1-gd' do
+    action :install
+end
+
+package 'php7.1-mbstring' do
+    action :install
+end
+
+package 'php7.1-fpm' do
+    action :install
+end
+
+package 'php7.1-mcrypt' do
+    action :install
+end
+
+package 'composer' do
+    action :install
+end
